8e1a202a5cd2b18a63552ec533da503c91945245
Onur Eren Arpaci <onurerenarpaci@hotmail.com>
2023-03-17 17:47:04 +0300

    add swagger

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require("swagger-jsdoc")
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const userRouter = require("./router/user");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Legal Research API",
+      version: "0.1.0",
+      description:
+        "This is the backend of the Legal Reseach application and documented with Swagger",
+      contact: {
+        name: "Onur Eren Arpaci",
+        email: "onurerenarpaci@hotmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./router/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 app.use(cors());
 app.use(express.json());
