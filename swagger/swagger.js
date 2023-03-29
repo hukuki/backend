e163ed7c7b7303c0a86611d8134cdb2fd8aaf2d0
Onur Eren Arpaci <onurerenarpaci@hotmail.com>
2023-03-30 00:45:04 +0300

    fix: swagger auth is configured an docker fixed

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -10,7 +10,7 @@ const doc = {
         email: "onurerenarpaci@hotmail.com",
         },
     },
-    host: "localhost:3000",
+    host: "localhost:8080",
     schemes: ["http"],
     components: {
         "@schemas": {
@@ -37,13 +37,19 @@ const doc = {
         },
         },
     },
+    securityDefinitions: {
+        bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT'
+        }
+    }
 };
 
-const outputFile = "./swagger-output.json";
-const endpointsFiles = ["./../index.js"];
+const outputFile = "./swagger/swagger-output.json";
+const endpointsFiles = ["./index.js"];
 
 /* NOTE: if you use the express Router, you must pass in the 
    'endpointsFiles' only the root file where the route starts,
    such as index.js, app.js, routes.js, ... */
-
 swaggerAutogen(outputFile, endpointsFiles, doc);
