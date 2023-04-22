c068a343e55dbeef1a933f64eafb1f44c5c37cad
Onur Eren Arpaci <onurerenarpaci@hotmail.com>
2023-04-22 20:06:06 +0300

    fix: update cred

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,19 +2,20 @@ const admin = require('firebase-admin');
 const { getAuth } = require('firebase-admin/auth');
 const User = require('../model/user');
 const Organization = require('../model/organization');
+const serviceAccount = require("../cred/cred.json");
 
-const serviceAccount = {
-    "type": process.env.FIREBASE_TYPE,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": new Buffer(process.env.FIREBASE_PRIVATE_KEY, 'base64').toString('ascii').replace(/\\n/gm, '\n'),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri": process.env.FIREBASE_AUTH_URI,
-    "token_uri": process.env.FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
-};
+// const serviceAccount = {
+//     "type": process.env.FIREBASE_TYPE,
+//     "project_id": process.env.FIREBASE_PROJECT_ID,
+//     "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+//     "private_key": new Buffer(process.env.FIREBASE_PRIVATE_KEY, 'base64').toString('ascii').replace(/\\n/gm, '\n'),
+//     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+//     "client_id": process.env.FIREBASE_CLIENT_ID,
+//     "auth_uri": process.env.FIREBASE_AUTH_URI,
+//     "token_uri": process.env.FIREBASE_TOKEN_URI,
+//     "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+//     "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+// };
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
