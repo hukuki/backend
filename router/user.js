57fe025f44988ef6a2176d3049625e71e27f63d2
erenyenigul <tc.eren.yenigul@gmail.com>
2023-04-25 17:12:27 +0300

    feat: return user on delete.

--- router/user.js
+++ router/user.js
@@ -36,13 +36,13 @@ router.delete("/:id", [auth, admin], verifyId, async (req, res) => {
 	const userId = req.params.id;
 	const organization = req.user.organization;
 
-	if(userId === req.user._id) return res.status(400).send({message: "You cannot delete yourself."});
+	if(userId == req.user._id) return res.status(400).send({message: "You cannot delete yourself."});
 
-	const {deletedCount} = await User.deleteOne({_id: userId, organization});
+	const user = await User.findOneAndDelete({_id: userId, organization});
 
-	if (deletedCount == 0) return res.status(404).send({message:"User not found."});
+	if (!user) return res.status(404).send({message:"User not found."});
 
-	res.send();
+	res.send(user);
 });
 
 module.exports = router;
