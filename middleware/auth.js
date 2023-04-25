fce837fb12b966339840f3e5ebe8d293b62b9965
erenyenigul <tc.eren.yenigul@gmail.com>
2023-04-25 20:37:17 +0300

    fix: wrong message.

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,18 +4,6 @@ const User = require('../model/user');
 const Organization = require('../model/organization');
 const serviceAccount = require("../cred/cred.json");
 
-// const serviceAccount = {
-//     "type": process.env.FIREBASE_TYPE,
-//     "project_id": process.env.FIREBASE_PROJECT_ID,
-//     "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-//     "private_key": new Buffer(process.env.FIREBASE_PRIVATE_KEY, 'base64').toString('ascii').replace(/\\n/gm, '\n'),
-//     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-//     "client_id": process.env.FIREBASE_CLIENT_ID,
-//     "auth_uri": process.env.FIREBASE_AUTH_URI,
-//     "token_uri": process.env.FIREBASE_TOKEN_URI,
-//     "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-//     "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
-// };
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -39,7 +27,7 @@ module.exports = async (req, res, next) => {
     try {
         userInfo = await admin.auth().verifyIdToken(accessToken);
     } catch (e) {
-        return res.status(401).send({ message: 'Unauthorized hello' });
+        return res.status(401).send({ message: 'Unauthorized' });
     }
 
     // We do not pass the user object retrieved from the third party auth provider.
