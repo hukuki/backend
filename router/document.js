26d534605b58995c01c297a56e71b1217e2646aa
erenyenigul <tc.eren.yenigul@gmail.com>
2023-05-17 00:53:49 +0300

    feat: document endpoint.

--- router/document.js
+++ router/document.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const s3 = require('../s3');
 const { mockDocumentData } = require('./util/mock');
+const {Utf8ArrayToStr} = require('./util/uint8');
 
 const router = express.Router();
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     // #swagger.summary = Get the content of a specific document.
-    res.send(mockDocumentData);
+
+    const file = await s3.getFile('mevzuat_json/'+req.params.id+'.json');
+    
+    if(!file) return res.status(404).send({message: 'Document not found.'} );
+    
+    const str = Utf8ArrayToStr(file);
+
+    res.send(str);
 });
 
 module.exports = router;
