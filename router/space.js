e4c67c0b953933e6fdc058b9a0b9e3dc7f7def55
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-07-15 11:11:40 +0300

    Space and User schemas are updated. New endpoints are added

--- router/space.js
+++ router/space.js
@@ -8,17 +8,35 @@ const { findOneSpace, findSpaces } = require("./util/space.js");
 
 const router = express.Router();
 
+// GET OPERATIONS:
 router.get("/", auth, async (req, res) => {
     // #swagger.summary = 'Get all the spaces which the user created or added to.'
     // #swagger.description = 'Spaces are models that represent a project/folder, in which `Note`s and `Bookmark`s within are related to a certain case or project. This feature is useful since user can group different stuff.
     const user = req.user._id;
-    const spaces = await Space.find({ "people.user":  user })
-    .populate('people')
-    .populate('createdBy')
-    .populate("documents");
+    const spaces = await Space.find({ "people.user":  user }).populate('createdBy')
     res.send(spaces);
 });
 
+
+router.get("/:spaceId/users", auth, verifyId, async (req, res) => {
+    // #swagger.summary = 'Get the people in a space'
+    // #swagger.description = 'Only people in a space can see the documents saved in that space'
+    const space = await Space.findOne({ _id: req.params.spaceId, "people.user": req.user._id})
+    res.send(space.people)
+
+})
+
+router.get("/:spaceId/documents", auth, verifyId, async (req, res) => {
+    // #swagger.summary = 'Get the documents in a space'
+    // #swagger.description = 'Only people in a space can see the documents saved in that space'
+    const space = await Space.findOne({ _id: req.params.spaceId, "people.user": req.user._id})
+    if (!space) return res.status(404).send({ message: "Space not found "})
+    res.send(space.documents)
+
+})
+
+
+// POST OPERATIONS
 router.post("/", auth, async (req, res) => {
     // #swagger.summary = 'Create a new space.'
 
@@ -37,42 +55,16 @@ router.post("/", auth, async (req, res) => {
     res.send(space);
 });
 
-router.delete("/:spaceId", auth, verifyId, async (req, res) => {
-    // #swagger.summary = 'Delete a space.'
-    // #swagger.description = 'Only people with the manager role can delete a space. The user created the space is assigned the manager role but further managers can change the role of the user who created the space'.
-
-    const space = await Space.findOne({ _id: req.params.spaceId, "people.user": req.user._id, "people.role": "manager"});
-    if (!space) {
-        res.status(400).send({ message: "Only managers can delete a space"})
-    }
-
-    const deletedSpace = await Space.deleteOne({ _id: req.params.spaceId })
-    await User.findOneAndUpdate({ _id: req.user._id }, { $pull: { spaces: req.params.spaceId } })
-    res.send(deletedSpace)
-});
-
 router.post("/:spaceId/users", auth, verifyId, async (req, res) => {
     // #swagger.summary = 'Add people to a space.'
     // #swagger.description = 'Only people with the manager role can add people to a space.'
     const space = await Space.findOne({ _id: req.params.spaceId, "people.user": req.user._id, "people.role": "manager"});
     if (!space) {
-        res.status(400).send({ message: "Only managers can add people to a space"})
+        return res.status(400).send({ message: "Only managers can add people to a space"})
     }
     const peopleToAdd = req.body.people;
     const updatedSpace = await Space.findOneAndUpdate({ _id: req.params.spaceId }, { $push: { people: { $each: peopleToAdd }} })
     await User.updateMany({ '_id': { $in: peopleToAdd } }, { $push: { spaces: req.params.spaceId }} )
-    res.send(updatedSpace)
-})
-
-router.put("/:spaceId/users/:userId", auth, verifyId, async(req, res) => {
-    // #swagger.summary = 'Change roles of people in a space.'
-    // #swagger.description = 'Only people with the manager role can change roles in a space.'
-    const space = await Space.findOne({ _id: req.params.spaceId, "people.user": req.user._id, "people.role": "manager"});
-    if (!space) {
-        res.status(400).send({ message: "Only managers can change roles in a space"})
-    }
-    const documentsToAdd = req.body.documents;
-    const updatedSpace = await Space.findOneAndUpdate({ _id: req.params.spaceId }, { $push: { documents: { $each: documentsToAdd }} })
     res.send(updatedSpace)
 })
 
@@ -81,24 +73,55 @@ router.post("/:spaceId/documents", auth, verifyId, async (req, res) => {
     // #swagger.description = 'Only people with the manager or editor roles can add documents to a space.'
     const space = await Space.findOne({ _id: req.params.spaceId, "people.user": req.user._id, "people.role": { $in: ["manager", "editor"]}});
     if (!space) {
-        res.status(400).send({ message: "Only managers and editors can add documents to a space"})
+        return res.status(400).send({ message: "Only managers and editors can add documents to a space"})
     }
     const documentsToAdd = req.body.documents;
-    const updatedSpace = await Space.findOneAndUpdate({ _id: req.params.spaceId }, { $push: { documents: { $each: documentsToAdd }} })
+    await Space.findOneAndUpdate({ _id: req.params.spaceId }, { $push: { documents: documentsToAdd }})
     res.send(updatedSpace)
 })
+
+//PUT OPERATIONS:
+router.put("/:spaceId/users/:userId", auth, verifyId, async(req, res) => {
+    // #swagger.summary = 'Change roles of a person in a space.'
+    // #swagger.description = 'Only people with the manager role can change roles in a space.'
+    const space = await Space.findOne({ _id: req.params.spaceId, "people.user": req.user._id, "people.role": "manager"});
+    if (!space) {
+        return res.status(400).send({ message: "Only managers can change roles in a space"})
+    }
+    const newRole = req.body.role;
+    if (newRole !== "observer" || newRole !== "manager" || newRole !== "editor") {
+        return res.status(400).send({ message: "Role not acceptable. "})
+    }
+    const updatedSpace = await Space.findOneAndUpdate({ _id: req.params.spaceId, "people.user": req.params.userId }, { $set: { "people.$.role": newRole }})
+    res.send(updatedSpace)
+})
+
+//DELETE OPERATIONS:
+
+router.delete("/:spaceId", auth, verifyId, async (req, res) => {
+    // #swagger.summary = 'Delete a space.'
+    // #swagger.description = 'Only people with the manager role can delete a space. The user created the space is assigned the manager role but further managers can change the role of the user who created the space'.
+
+    const space = await Space.findOne({ _id: req.params.spaceId, "people.user": req.user._id, "people.role": "manager"});
+    if (!space) {
+        return res.status(400).send({ message: "Only managers can delete a space"})
+    }
+
+    const deletedSpace = await Space.deleteOne({ _id: req.params.spaceId })
+    const users = space.people.map((p) => p.user)
+    await User.updateMany({ '_id': { $in: users } }, { $pull: { spaces: req.params.spaceId }} )
+    res.send(deletedSpace)
+});
 
 router.delete("/:spaceId/users/:userId", auth, verifyId, async (req, res) => {
     // #swagger.summary = 'Delete a person from a space.'
     // #swagger.description = 'Only people with the manager role can delete a person from a space.'.
     const space = await Space.findOne({ _id: req.params.spaceId, "people.user": req.user._id, "people.role": "manager"});
     if (!space) {
-        res.status(400).send({ message: "Only managers can delete a space"})
+        return res.status(400).send({ message: "Only managers can delete a space"})
     }
 
     const deletedUserId = req.params.userId;
-    const userId = req.user._id;
-
     const updatedSpace = await Space.findOneAndUpdate({ _id: req.params.spaceId }, { $pull: { people: deletedUserId } })
     await User.findOneAndUpdate({ _id: deletedUserId }, { $pull: { spaces: deletedUserId }})
     res.send(updatedSpace)
@@ -109,7 +132,7 @@ router.delete("/:spaceId/users/:documentId", auth, verifyId, async (req, res) =>
     // #swagger.description = 'Only people with the manager and editor roles can delete a document from a space.'.
     const space = await Space.findOne({ _id: req.params.spaceId, "people.user": req.user._id, "people.role": { $in: ["manager", "editor"]}});
     if (!space) {
-        res.status(400).send({ message: "Only managers and editors can delete a document from a space"})
+        return res.status(400).send({ message: "Only managers and editors can delete a document from a space"})
     }
 
     const documentId = req.params.documentId;
@@ -117,119 +140,5 @@ router.delete("/:spaceId/users/:documentId", auth, verifyId, async (req, res) =>
     res.send(updatedSpace)
 })
 
-/*
-router.get("/:spaceId/notes", auth, async (req, res) => {
-    // #swagger.summary = 'Get all the notes in a space.'
 
-    const space = await findOneSpace({ space: req.params.spaceId, user: req.user._id });
-
-
-    if (!space) return res.status(404).send({ message: "Space not found." });
-
-    console.log(space);
-
-    const notes = await Note.find({ space: space._id }).populate('user');
-
-    res.send(notes);
-});
-*/
-
-router.get("/:spaceId/users", auth, verifyId, async (req, res) => {
-    // #swagger.summary = 'Get the people in a space'
-
-    // Get the space which user created or has been added to
-    const space = await Space.findOne({ _id: req.params.spaceId}).populate("people")
-
-    if (!space) return res.status(404).send({ message: "Space not found "})
-
-    const user = req.user._id
-
-    if (!space.people.get(user)) {
-        return res.status(401).send({ message: "Unauthorized"})
-    }
-
-    res.send(space.people)
-
-})
-
-/*
-router.post("/:spaceId/users", auth, verifyId, async (req, res) => {
-    // #swagger.summary = 'Add a list of people to a space'
-
-    if (!req.body.people || req.body.people.length === 0) {
-        return res.status(400).send({ message: "People in body cannot be empty "})
-    }
-
-    const users = await User.find({ _id: { $in: req.body.people }})
-    const userIds = users.map((user) => user._id)
-
-    const space = await Space.findOne({ _id: req.params.spaceId, user: req.user._id }).populate("people").populate("user")
-
-    if (!space) res.status(404).send({ message: "Space not found "})
-
-    const userIdsInSpace = space.people.map((person) => person._id)
-
-
-})
-
-router.post("/:spaceId/users/:userId", auth, verifyId, async (req, res) => {
-    // #swagger.summary = 'Add a collegue to a space.'
-
-    const user = await User.findOne({ _id: req.params.userId, organization: req.user.organization });
-
-    // Here, we don't use the helper findOneSpace, because only the owner of the space
-    // can add a user to the space.
-
-    const space = await Space.findOne({ _id: req.params.spaceId, user: req.user._id })
-        .populate('people')
-        .populate('user');
-
-    if (!space) return res.status(404).send({ message: "Space not found." });
-
-    space.people = space.people.filter((item) => {
-        console.log(item);
-        return item._id != user._id;
-    });
-
-    space.people.pull({ _id: user._id });
-    space.people.push(user);
-
-    await space.save();
-
-    res.send(space);
-});
-
-router.delete("/:spaceId/users/:userId", auth, verifyId, async (req, res) => {
-    // #swagger.summary = 'Delete a collegue from the space.'
-
-    const user = await User.findOne({ _id: req.params.userId, organization: req.user.organization });
-
-    // Here, we don't use the helper findOneSpace,
-    // because only the owner can delete an user from the space.
-
-    const space = await Space.findOne({ _id: req.params.spaceId, user: req.user._id })
-        .populate('people')
-        .populate('user');
-
-    if (!space) return res.status(404).send({ message: "Space not found." });
-
-    space.people.pull({ _id: user._id });
-    await space.save();
-
-    res.send(space);
-});
-
-router.get("/:spaceId/bookmarks", auth, async (req, res) => {
-    // #swagger.summary = 'Get all the bookmarks in a space.'
-
-    const space = await findOneSpace({ space: req.params.spaceId, user: req.user._id });
-
-    if (!space) return res.status(404).send({ message: "Space not found." });
-
-    const bookmarks = await Bookmark.find({ space: space._id }).populate('user');;
-
-    res.send(bookmarks);
-});
-
-*/
 module.exports = router;
--- router/user.js
+++ router/user.js
@@ -1,5 +1,6 @@
 const User = require("../model/user");
 const Document = require("../model/document")
+const Space = require("../model/space");
 const express = require("express");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
@@ -7,12 +8,14 @@ const verifyId = require("../middleware/verifyId");
 const router = express.Router();
 
 // Path: backend/router/user.js
+
+// GET OPERATIONS
 router.get("/", auth, async (req, res) => {
 	// #swagger.summary = Get all the collegues of the user.
 
 	const organization = req.user.organization;
 
-	const users = await User.find({ organization: organization }).populate("bookmarks", Document);
+	const users = await User.find({ organization: organization }).populate("bookmarks", Document).populate("spaces", Space);
 
 	res.send(users);
 });
@@ -23,12 +26,73 @@ router.get("/:id", auth, verifyId, async (req, res) => {
 	const userId = req.params.id;
 	const organization = req.user.organization;
 
-	const user = await User.findOne({ _id: userId, organization });
+	const user = await User.findOne({ _id: userId, organization }).populate("bookmarks", Document).populate("spaces", Space);
 
 	if (!user) return res.status(404).send({message:"User not found."});
 
 	res.send(user);
 });
+
+router.get("/bookmarks", auth, verifyId, async (req, res) => {
+	// #swagger.summary = Get bookmarks of the user.
+
+	const userId = req.user._id;
+	const user = await User.findOne({ _id: userId }).populate("bookmarks", Document)
+	if (!user) return res.status(404).send({message:"User not found."});
+	res.send(user.bookmarks)
+})
+
+router.get("/spaces", auth, verifyId, async (req, res) => {
+	// #swagger.summary = Get spaces of the user.
+	const userId = req.user._id;
+	const user = await User.findOne({ _id: userId }).populate("spaces", Space)
+	if (!user) return res.status(404).send({message:"User not found."});
+	res.send(user.spaces)
+})
+
+// POST OPERATIONS:
+router.post("/bookmarks", auth, verifyId, async (req, res) => {
+	// #swagger.summary = Add bookmarks to the user.
+	const userId = req.user._id;
+	const user = await User.findOne({ _id: userId })
+	if (!user) return res.status(404).send({message:"User not found."});
+	const bookmarks = req.body.bookmarks;
+	const updatedUser = await User.findOneAndUpdate({ _id: req.user._id }, { $push: { bookmarks: bookmarks }})
+	res.send(updatedUser)
+})
+
+router.post("/spaces", auth, verifyId, async (req, res) => {
+	// #swagger.summary = Add spaces to the user.
+	const userId = req.user._id;
+	const user = await User.findOne({ _id: userId })
+	if (!user) return res.status(404).send({message:"User not found."});
+	const spaces = req.body.spaces;
+	const updatedUser = User.findOneAndUpdate({ _id: userId }, { $push: { spaces: spaces }})
+	await Space.updateMany({ "_id": { $in: spaces }}, { $push: { people: userId}})
+	res.send(updatedUser)
+})
+
+// DELETE OPERATIONS:
+router.delete("/bookmarks/:bookmarkId", auth, verifyId, async (req, res) => {
+	// #swagger.summary = Delete a bookmark from the user.
+	const userId = req.user._id;
+	const user = await User.findOne({ _id: userId })
+	if (!user) return res.status(404).send({message:"User not found."});
+	const bookmarkId = req.params.bookmarkId;
+	const updatedUser = User.findOneAndUpdate({ _id: userId }, { $pull: { bookmarks: bookmarkId }})
+	res.send(updatedUser)
+})
+
+router.delete("/spaces/:spaceId", auth, verifyId, async (req, res) => {
+	// #swagger.summary = Delete a space from the user.
+	const userId = req.user._id;
+	const user = await User.findOne({ _id: userId })
+	if (!user) return res.status(404).send({message:"User not found."});
+	const spaceId = req.params.spaceId;
+	const updatedUser = User.findOneAndUpdate({ _id: userId }, { $pull: { spaces: spaceId }})
+	await Space.updateMany({ "people.user": userId }, { $pull: { people: userId }})
+	res.send(updatedUser)
+})
 
 router.delete("/:id", [auth, admin], verifyId, async (req, res) => {
 	// #swagger.summary = Delete a certain collegue account. Needs domain-admin access.
@@ -41,6 +105,8 @@ router.delete("/:id", [auth, admin], verifyId, async (req, res) => {
 	const user = await User.findOneAndDelete({_id: userId, organization});
 
 	if (!user) return res.status(404).send({message:"User not found."});
+
+	await Space.updateMany({ "people.user" : userId }, { $pull: { people: userId }})
 
 	res.send(user);
 });
