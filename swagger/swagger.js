601f282b6aa792108fe04b9d72a77b6c681630c5
Onur Eren Arpaci <onurerenarpaci@hotmail.com>
2023-08-26 00:36:17 -0400

    fix: swagger autogen

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -110,7 +110,7 @@ const doc = {
 };
 
 const outputFile = "./swagger/swagger-output.json";
-const endpointsFiles = ["./index.js"];
+const endpointsFiles = ["./index.js", "./src/app.js"];
 
 /* NOTE: if you use the express Router, you must pass in the 
    'endpointsFiles' only the root file where the route starts,
